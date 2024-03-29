77dfcc8f79e883c8ec4989b90c29b79e60119f39
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-29 11:53:59 +0100

    commit

--- handle_specifier.c
+++ handle_specifier.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
  * handle_specifier - Handle format specifier and call corresponding function.
- *
  * @format_specifier: The format specifier to handle.
  * @args: The list of arguments.
- *
  * Return: int.
  */
 int handle_specifier(char format_specifier, va_list args)
 {
 int i = 0, j = 0;
-
 ConversionHandler_t format_handlers[] = {
 {'c', my_char_printer},
 {'s', my_string_printer},
@@ -20,7 +17,6 @@ ConversionHandler_t format_handlers[] = {
 {'r', my_reverse_string_printer},
 {0, NULL}
 };
-
 while (format_handlers[i].specifier != 0)
 {
 if (format_specifier == format_handlers[i].specifier)
@@ -30,12 +26,10 @@ break;
 }
 i++;
 }
-
 if (j == 0)
 {
 j += _putchar('%');
 j += _putchar(format_specifier);
 }
-
 return (j);
 }
