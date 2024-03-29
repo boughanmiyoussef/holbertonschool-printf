50991ce77be460cebc31365dff12bd8f6f9daad7
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-29 11:43:42 +0100

    commit

--- _printf.c
+++ _printf.c
@@ -9,55 +9,56 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+va_list args;
+int count = 0;
 
-    if (!format)
-        return (-1);
+if (!format)
+return (-1);
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '\0')
-                return (-1);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
 
-            if (*format == '%')
-            {
-                count += _putchar('%');
-            }
-            else if (*format == 'c')
-            {
-                count += _putchar(va_arg(args, int));
-            }
-            else if (*format == 's')
-            {
-                char *s = va_arg(args, char *);
-                if (s == NULL)
-                    s = "(null)";
-                while (*s)
-                {
-                    count += _putchar(*s);
-                    s++;
-                }
-            }
-            else
-            {
-                count += _putchar('%');
-                count += _putchar(*format);
-            }
-        }
-        else
-        {
-            count += _putchar(*format);
-        }
-        format++;
-    }
+if (*format == '\0')
+return (-1);
 
-    va_end(args);
+if (*format == '%')
+{
+count += _putchar('%');
+}
+else if (*format == 'c')
+{
+count += _putchar(va_arg(args, int));
+}
+else if (*format == 's')
+{
+char *s = va_arg(args, char *);
+if (s == NULL)
+s = "(null)";
+while (*s)
+{
+count += _putchar(*s);
+s++;
+}
+}
+else
+{
+count += _putchar('%');
+count += _putchar(*format);
+}
+}
+else
+{
+count += _putchar(*format);
+}
+format++;
+}
 
-    return (count);
+va_end(args);
+
+return (count);
 }
