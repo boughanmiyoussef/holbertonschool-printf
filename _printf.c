ec56cb754db39a777d33e8378a71a4892ce288f2
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-30 17:43:57 +0100

    Added custom printf function to emulate the behavior of standard printf

--- _printf.c
+++ _printf.c
@@ -10,40 +10,52 @@ int _printf(const char *format, ...)
 {
 va_list args;
 int count = 0;
+
 if (!format)
 return (-1);
+
 va_start(args, format);
+
 while (*format)
 {
 if (*format == '%')
 {
 format++;
+
 if (*format == '\0')
 return (-1);
+
 if (*format == '%')
 {
 count += _putchar('%');
 }
+
 else if (*format == 'c')
 {
 count += _putchar(va_arg(args, int));
 }
+
 else if (*format == 's')
 {
+
 char *s = va_arg(args, char *);
+
 if (s == NULL)
 s = "(null)";
+
 while (*s)
 {
 count += _putchar(*s);
 s++;
 }
 }
+
 else
 {
 count += _putchar('%');
 count += _putchar(*format);
 }
+
 }
 else
 {
@@ -51,6 +63,7 @@ count += _putchar(*format);
 }
 format++;
 }
+
 va_end(args);
 return (count);
 }
