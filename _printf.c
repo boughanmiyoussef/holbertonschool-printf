0e06e7596978d547280f3e2123db75215055e9b3
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-04-01 13:25:03 +0100

    Betty style correction

--- _printf.c
+++ _printf.c
@@ -8,51 +8,27 @@ int _printf(const char *format, ...)
 {
 va_list args;
 int count = 0;
-if (!format)
-return (-1);
-
+if (!format) return (-1);
 va_start(args, format);
-
 while (*format)
 {
 if (*format == '%')
 {
 format++;
-
-if (*format == '\0')
-return (-1);
-
-if (*format == '%')
-{
-count += _putchar('%');
-}
-
-else if (*format == 'c')
-{
-count += _putchar(va_arg(args, int));
-}
-
+if (*format == '\0') return (-1);
+if (*format == '%') count += _putchar('%');
+else if (*format == 'c') count += _putchar(va_arg(args, int));
 else if (*format == 's')
 {
-
 char *s = va_arg(args, char *);
-
-if (s == NULL)
-s = "(null)";
-
-while (*s)
-{
-count += _putchar(*s);
-s++;
+if (s == NULL) s = "(null)";
+while (*s) count += _putchar(*s++);
 }
-}
-
 else
 {
 count += _putchar('%');
 count += _putchar(*format);
 }
-
 }
 else
 {
@@ -60,7 +36,6 @@ count += _putchar(*format);
 }
 format++;
 }
-
 va_end(args);
 return (count);
 }
